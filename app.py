16a2728ac7bf10eea0077f45364e37aa6442be84
guepardlover77 <89525425+guepardlover77@users.noreply.github.com>
2024-09-07 18:29:49 +0200

    Update app.py

--- app.py
+++ app.py
@@ -72,10 +72,6 @@ def admin_interface(qcm_data):
 
 def main():
     qcm_data = load_qcm()
-
-    st.sidebar.title("Bienvenue dans l'application QCM")
-
-    # Set default page to "Utilisateur" instead of "Administration"
     page = st.sidebar.selectbox("Navigation", ["Utilisateur", "Administration", "FAQ"])
 
     if page == "Administration":
